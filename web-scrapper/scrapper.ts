769a539fcb1d8070994851595bf961049d210e34
Tomáš Hanzelka <hanzelkatomas3@gmail.com>
2023-11-05 21:23:46 +0100

    Finish Dockerfile for working puppeteer

--- web-scrapper/scrapper.ts
+++ web-scrapper/scrapper.ts
@@ -3,12 +3,15 @@ import puppeteer, {Page} from "puppeteer";
 
 export const getFlatSellOffers = async (scrapUrl: string, offersPerPage: number, itemsToScrap: number) => {
   const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    executablePath: "/bin/google-chrome",
+    headless: "new",
+    slowMo: 200,
+    executablePath: "/opt/google/chrome/google-chrome",
+    // For working in headless mode
+    args: ["--no-sandbox", "--disable-setuid-sandbox", "window-size=1920,1080"],
   });
 
   const page = await browser.newPage();
+  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
   let totalOffers = []
 
   const pagesToScrap = Math.ceil(itemsToScrap / offersPerPage)
@@ -18,9 +21,8 @@ export const getFlatSellOffers = async (scrapUrl: string, offersPerPage: number,
     totalOffers = [...totalOffers, ...pageOffersTmp]
   }
 
+  await browser.close();
   return totalOffers
-  // TODO - close browser
-  // await browser.close();
 }
 
 async function scrapPage(i: number, page: Page, scrapUrl: string) {
